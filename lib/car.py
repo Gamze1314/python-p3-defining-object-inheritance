8a8ac6f4a89d778e14910bd4494300fa64982a0b
gamze <gamzekandara@gmail.com>
2024-05-17 22:55:50 -0400

    Adds car.py

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,13 @@
-from vehicle import Vehicle
+from vehicle import Vehicle  #configuration
 
+#subclass of Vehicle
+#Car class inherit all of the Vehicle attributes and methods and therefore have access to them. 
 class Car(Vehicle):
-    pass
+    # overwrite the inherited go() method with one specific to Car.
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+
+
+my_car2 = Car("bigggg" , 3)
+print(my_car2.go())
+print(Car.__bases__)  # (<class 'vehicle.Vehicle'>,) => returns tuple, parent classess of Car class
--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,18 @@
 class Vehicle:
-    pass
+    
+    def __init__(self, wheel_size, wheel_number):
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+
+
+    def go(self):
+        return "vrrrrrrrooom!"
+
+
+    def fill_up_tank(self):
+        return "filling up!"
+
+
+my_car = Vehicle("4-passenger seated" , 2)
+print(my_car.go())
+print(my_car.fill_up_tank())
